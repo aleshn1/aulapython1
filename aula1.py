d208731f9c5f7ab910c82f076a27ff153f630673
aleshn1 <122236649+aleshn1@users.noreply.github.com>
2023-01-12 00:41:17 -0300

    atualtres

--- aula1.py
+++ aula1.py
@@ -1,2 +1,16 @@
 #vamor comecar a aula 1
-print ("olá tudo bem ")
+import webbrowser as wb
+import pyautogui
+import pyperclip
+import time
+
+pyautogui.PAUSE=2
+wb.open('www.google.com')
+time.sleep(2)
+pyautogui.hotkey('ctrl','t')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+time.sleep(2)
+pyautogui.hotkey('ctrl','v')
+pyautogui.hotkey('enter')
+
+
