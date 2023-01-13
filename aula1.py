6785d547346281d84f51f014515ef99b1edec657
aleshn1 <122236649+aleshn1@users.noreply.github.com>
2023-01-13 20:01:59 -0300

    atulizar

--- aula1.py
+++ aula1.py
@@ -4,14 +4,21 @@ import pyautogui
 import pyperclip
 import time
 
+
 pyautogui.PAUSE=2
 wb.open('www.google.com')
+time.sleep(3)
+pyautogui.click(x=1169,y=136)
+time.sleep(4)
+pyautogui.click(x=76,y=196)
 time.sleep(2)
-pyautogui.hotkey('ctrl','t')
-pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
-time.sleep(2)
+pyautogui.click(x=868,y=308)
+time.sleep(1)
+pyperclip.copy('alessandrohn@hotmail.com')
 pyautogui.hotkey('ctrl','v')
-pyautogui.hotkey('enter')
+pyautogui.click(x=863,y=369)
+pyperclip.copy('Segue o relatório de faturamento de vendas')
+pyautogui.hotkey('ctrl','v')
 
 #proxima etapa
 
