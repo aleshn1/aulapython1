44eb47943bedb653efad43d3a8c38c92c397ec7b
aleshn1 <122236649+aleshn1@users.noreply.github.com>
2023-01-14 16:10:44 -0300

    autmatizar-ok

---
+++ automacao.py
@@ -0,0 +1,72 @@
+#vamor comecar a aula 1
+import webbrowser as wb
+import pyautogui
+import pyperclip
+import time
+import pandas as pd 
+
+print('Processando...')
+time.sleep(3)
+pyautogui.PAUSE=2
+wb.open('www.google.com')
+time.sleep(2)
+pyautogui.hotkey('ctrl','t')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+time.sleep(2)
+pyautogui.hotkey('ctrl','v')
+pyautogui.hotkey('enter')
+
+
+time.sleep(2)
+#print(pyautogui.position())
+pyautogui.click(x=338, y=270, clicks=2)
+time.sleep(3)
+#tela do excel
+pyautogui.click(x=462, y=394)
+time.sleep(2)
+#tres pontinhos
+pyautogui.click(x=818, y=159)
+#fazer dowload
+pyautogui.click(x=619, y=557)
+time.sleep(3)
+
+# proxima fazer baixar o excel
+tabela = pd.read_excel(r'C:\Users\Windows\Downloads\Vendas - Dez.xlsx')
+faturamento = tabela ['Valor Final'].sum()
+quantidade = tabela ['Quantidade'].sum()
+print(faturamento,quantidade)
+
+time.sleep(2)
+pyautogui.PAUSE=2
+wb.open('www.google.com')
+time.sleep(1)
+pyautogui.click(x=806, y=99)
+time.sleep(1)
+pyautogui.click(x=73, y=168)
+time.sleep(4)
+pyautogui.click(x=593, y=251)
+time.sleep(2)
+pyautogui.write('alessandrohn@hotmail.com')
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(1)
+pyperclip.copy('Relatório de Vendas')
+pyautogui.hotkey('ctrl','v')
+pyautogui.press('tab')
+#Dados do relatorios ser enviado.
+texto = (f'''
+
+Prezados, segue os dados sobre o faturamento.
+
+O faturamento de ontem foi de: {faturamento:,.2f} 
+A quantidade de produtos foi de: {quantidade:,.2f}
+
+Att.ale
+''')
+pyperclip.copy(texto)
+pyautogui.hotkey('ctrl','v')
+time.sleep(1)
+pyautogui.hotkey('ctrl','enter')
+print("E-mail enviado com sucesso.")
+confirmação = input('Deseja realizar novamente ?')
+
